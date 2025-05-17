c1e59af197caf9ebf3d0a04858deaa0ed38b04a0
ChanGitdeploy <165387585+ChanGitdeploy@users.noreply.github.com>
2025-05-17 17:32:02 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,48 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About | www.employee.com</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="wrapper" style="width:600px; margin:0 auto; margin-top:50px;">
+        <h2>ABOUT ME</h2>
+
+        <table>
+            <tr>
+                <td><strong>Name:</strong></td>
+                <td>Nu Nechan</td>
+            </tr>
+             <tr>
+                <td><strong>Email:</strong></td>
+                <td>nu.nechan.1222@rupp.edu.kh</td>
+            </tr>
+             <tr>
+                <td><strong>Class:</strong></td>
+                <td>A4B-109</td>
+            </tr>
+            <tr>
+                <td><strong>Gender:</strong></td>
+                <td>Male</td>
+            </tr>
+             <tr>
+                <td><strong>Major:</strong></td>
+                <td>Computer Science</td>
+            </tr>
+             <tr>
+                <td><strong>University:</strong></td>
+                <td>ROYAL UNIVERSITY OF PHNOM PENH
+</td>
+            </tr>
+               
+
+
+
+        </table>
+        <a href="index.php" class="btn btn-primary">Back to Home</a>
+    </div>
+</body>
+</html>
